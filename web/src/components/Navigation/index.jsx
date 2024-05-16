6d90c187452a4da42eb44ee72b8b2c42a5801fb6
lucthanca <lucthanca@gmail.com>
2024-05-16 18:05:17 +0700

    fix scroll to top on navigation shopify

--- web/src/components/Navigation/index.jsx
+++ web/src/components/Navigation/index.jsx
@@ -1,10 +1,24 @@
-import { memo, useCallback } from 'react';
-import { NavigationMenu } from '@shopify/app-bridge-react';
+import { memo, useCallback, useEffect } from 'react';
+import { NavigationMenu, useAppBridge } from '@shopify/app-bridge-react';
 import { useNavigationLinks } from '~/components/Navigation/links';
+import { Redirect } from '@shopify/app-bridge/actions';
 
 const Nav = () => {
   const links = useNavigationLinks();
   const matcher = useCallback((link, location) => link.destination === location.pathname, []);
+  const app = useAppBridge();
+  const scrollToTop = () => {
+    const c = document.documentElement.scrollTop || document.body.scrollTop;
+    if (c > 0) {
+      window.requestAnimationFrame(scrollToTop);
+      window.scrollTo(0, c - c / 8);
+    }
+  };
+  useEffect(() => {
+    app.subscribe(Redirect.Action.APP, function(redirectData) {
+      scrollToTop();
+    });
+  }, []);
   return <NavigationMenu navigationLinks={links} matcher={matcher} />;
 };
 
