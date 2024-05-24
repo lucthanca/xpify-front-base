9df37fd62b36ae5cbd9f420ae489d8944fbb0316
X-LongND2 <longnd2@bsscommerce.com>
2024-05-24 17:35:13 +0700

    Update js and css for app

--- extensions/active-section-bss/assets/otsb-script.js
+++ extensions/active-section-bss/assets/otsb-script.js
@@ -488,6 +488,23 @@ requestAnimationFrame(() => {
             }
           }
       }));
+      Alpine.data("xMap", (data) => ({
+        load() {
+          this.$el.querySelector(
+            "iframe"
+          ).src = `https://maps.google.com/maps?q=${data}&t=m&z=17&ie=UTF8&output=embed&iwloc=near`;
+        },
+        loadMap(location) {
+          this.$el.querySelector(
+            "iframe"
+          ).src = `https://maps.google.com/maps?q=${location}&t=m&z=17&ie=UTF8&output=embed&iwloc=near`;
+        },
+        removeMap() {
+          this.$el.querySelector(
+            "iframe"
+          ).src = ``;
+        } 
+      }));
   })
 });
 
