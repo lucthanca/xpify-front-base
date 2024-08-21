f43fab533bcd6030adb00d08f472b02ce5ea3114
lucthanca <lucthanca@gmail.com>
2024-08-21 14:51:50 +0700

    otsb suffix funcname

--- extensions/active-section-bss/assets/otsb-script.js
+++ extensions/active-section-bss/assets/otsb-script.js
@@ -360,7 +360,7 @@ requestAnimationFrame(() => {
               };
 
               if (!window.OTSB.loadedScript.includes('slider')) {
-                  deferScriptLoad('slider', window.OTSB.sliderScript, initSlider, true);
+                  deferScriptLoadOTSB('slider', window.OTSB.sliderScript, initSlider, true);
               } else if (window.Splide) {
                   initSlider();
               } else {
@@ -674,7 +674,8 @@ requestAnimationFrame(() => {
   })
 });
 
-const deferScriptLoad = (name, src, onload, requestVisualChange = false) => {
+if (typeof window.deferScriptLoadOTSB != 'function') {
+window.deferScriptLoadOTSB = (name, src, onload, requestVisualChange = false) => {
   window.OTSB.loadedScript.push(name);
 
   ((events) => {
@@ -717,6 +718,7 @@ const deferScriptLoad = (name, src, onload, requestVisualChange = false) => {
       }
   })(['touchstart', 'mouseover', 'wheel', 'scroll', 'keydown']);
 };
+}
 
 /** intersect 3.10.5 */
 requestAnimationFrame(() => {(()=>{function c(e){e.directive("intersect",(t,{value:i,expression:l,modifiers:n},{evaluateLater:r,cleanup:o})=>{let s=r(l),d={rootMargin:p(n),threshold:f(n)},u=new IntersectionObserver(h=>{h.forEach(a=>{a.isIntersecting!==(i==="leave")&&(s(),n.includes("once")&&u.disconnect())})},d);u.observe(t),o(()=>{u.disconnect()})})}function f(e){if(e.includes("full"))return .99;if(e.includes("half"))return .5;if(!e.includes("threshold"))return 0;let t=e[e.indexOf("threshold")+1];return t==="100"?1:t==="0"?0:Number(`.${t}`)}function x(e){let t=e.match(/^(-?[0-9]+)(px|%)?$/);return t?t[1]+(t[2]||"px"):void 0}function p(e){let t="margin",i="0px 0px 0px 0px",l=e.indexOf(t);if(l===-1)return i;let n=[];for(let r=1;r<5;r++)n.push(x(e[l+r]||""));return n=n.filter(r=>r!==void 0),n.length?n.join(" ").trim():i}document.addEventListener("alpine:init",()=>{window.Alpine.plugin(c)});})();});
