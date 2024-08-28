0986ab64bd48b15cbcb47048ea24f4a97e040794
lucthanca <lucthanca@gmail.com>
2024-08-28 18:49:19 +0700

    fix bug

--- web/src/Apollo/links/dataCacheLink.ts
+++ web/src/Apollo/links/dataCacheLink.ts
@@ -7,11 +7,14 @@ export default () => {
   return new ApolloLink((operation, forward) => {
     return forward(operation).map(response => {
       const context = operation.getContext();
+      if (!context) return response;
       const { response: { headers }, cache } = context;
       const clientVer = storage.getItem(CLIENT_CACHE_KEY);
       const serverVer = headers.get(CLIENT_CACHE_KEY);
       if (clientVer !== serverVer) {
-        const rootQuery = cache.data.data.ROOT_QUERY;
+        const rootQuery = cache?.data?.data?.ROOT_QUERY;
+        if (rootQuery === null || rootQuery === undefined || typeof rootQuery !== 'object') return response;
+
         Object.keys(rootQuery).forEach(key => {
           if (key.startsWith('getSections')) {
             cache.evict({ id: 'ROOT_QUERY', 'fieldName': key });
